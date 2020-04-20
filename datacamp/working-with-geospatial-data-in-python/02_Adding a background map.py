a0e1434ef7426c4170f86612750b3e4e57f5df80
2innnnn0 <2innnnn0@kakao.com>
2020-04-21 00:18:16 +0900

    [upload] geopandas

--- datacamp/working-with-geospatial-data-in-python/02_Adding a background map.py
+++ datacamp/working-with-geospatial-data-in-python/02_Adding a background map.py
@@ -10,6 +10,11 @@ restaurants.head()
 
 # A figure of all restaurants with background
 fig, ax = plt.subplots()
-ax.plot(restaurants['x'], restaurants['y'], 'o', 1)
+ax.plot(restaurants['x'], restaurants['y'], 'o', markersize=1)
 contextily.add_basemap(ax)
 plt.show()
+
+
+
+# ---
+# contextily패키지에서 add_basemap()는 백그라운드 맵을 쉽게 그려주는 함수다. 
---
+++ datacamp/working-with-geospatial-data-in-python/03_Introduction to Geopandas.py
@@ -0,0 +1,18 @@
+# 03_Introduction to Geopandas.py
+
+# 1. importing geospatial Data with geopandas
+# geopandas는 geojson, shp파일 등을 지원한다.
+# geopandas의 타입은 geopandas.geodataframe.GeoDataFrame 이다.
+# geopandas. geometry로 하면 자동으로 지리데이터를 인식한다.
+# geometry속성의 타입은 geopandas.geosereis.GeoSeires 이다.
+# geometry.area 는 면적값을 float으로 반환한다.
+
+# geoDataFrame은 판다스의 데이터프레임과 같다. 
+
+
+import geopandas
+contries = geopandas.read_file("contries.geojson")
+contries.head()
+
+# contries에서 
+contries.plot()
