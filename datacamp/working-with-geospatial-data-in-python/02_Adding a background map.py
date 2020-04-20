44bf7ce8ec899daaa849c6991b16446005a93322
2innnnn0 <2innnnn0@kakao.com>
2020-04-20 23:59:06 +0900

    Create 02_Adding a background map.py

---
+++ datacamp/working-with-geospatial-data-in-python/02_Adding a background map.py
@@ -0,0 +1,15 @@
+# 02_Adding a background map.py
+
+# Read the restaurants csv file
+restaurants = pd.read_csv("paris_restaurants.csv")
+
+# Import contextily
+import contextily
+
+restaurants.head()
+
+# A figure of all restaurants with background
+fig, ax = plt.subplots()
+ax.plot(restaurants['x'], restaurants['y'], 'o', 1)
+contextily.add_basemap(ax)
+plt.show()
