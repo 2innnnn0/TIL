62c00812e6b3ae9843ba4c7c3c79c2ae292072ed
이인영 <jkikss@naver.com>
2020-04-30 23:58:56 +0900

    Create 14_spatial_relationships_with_Geopandas.py

---
+++ datacamp/working-with-geospatial-data-in-python/14_spatial_relationships_with_Geopandas.py
@@ -0,0 +1,24 @@
+# 14_spatial_relationships_with_Geopandas.py
+
+
+# element-wise spatial relationship methods.
+brussels.within(france)
+>> False
+
+paris.within(france)
+>> True
+
+cities.head()
+
+# The within() operation for each geometry in cities:
+# Data시리즈로 비교하는것과 인덱스로 하나씩 비교 하는 내용.
+
+
+# 
+# france를 포함하는 도시들 모음 
+cities[cities.within(france)]
+
+# 공간 관계로 필터링
+within
+contains
+intersects
